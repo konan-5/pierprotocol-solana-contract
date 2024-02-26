2aa387afab35bc65d6b09b2e84ad2a39777c79c4
konan-5 <ikedahiroshi517@gmail.com>
2024-02-26 01:41:44 +0000

    update lib

--- programs/piersol/src/lib.rs
+++ programs/piersol/src/lib.rs
@@ -10,7 +10,15 @@ declare_id!("BFyTZKQiRPnk8AkBEfD1mhm5ZGuq46GWXg5wbFAYPzPx");
 pub mod piersol {
     use super::*;
 
+    pub fn initialize_config(ctx: Context<InitializeConfigPdaCtx>) -> Result<()> {
+        initialize_pdas::initialize_config_handler(ctx)
+    }
+
     pub fn initialize_book(ctx: Context<InitializeBookPdaCtx>) -> Result<()> {
         initialize_pdas::initialize_book_handler(ctx)
+    }
+
+    pub fn initialize_escrow(ctx: Context<InitializeEscrowPdaCtx>) -> Result<()> {
+        initialize_pdas:: initialize_escrow_handler(ctx)
     }
 }
